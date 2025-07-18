502920af29e41535e6e5da0f880a8741b901ba14
unggun <andreas.unggun@sirclo.com>
2025-07-18 10:11:36 +0700

    Add Vercel analytics and speed insights: integrate @vercel/analytics and @vercel/speed-insights into layout and update package dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ import classNames from "classnames";
 import { Background, Column, Flex, Meta, opacity, SpacingToken } from "@once-ui-system/core";
 import { Footer, Header, RouteGuard, Providers } from '@/components';
 import { baseURL, effects, fonts, style, dataStyle, home } from '@/resources';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 export async function generateMetadata() {
   return Meta.generate({
@@ -155,6 +158,8 @@ export default async function RootLayout({
             </Flex>
             <Footer/>
           </Column>
+          <Analytics />
+          <SpeedInsights />
         </Providers>
       </Flex>
   );
